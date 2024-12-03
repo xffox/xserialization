e741b03350da937c84e66d0990d141897a40cacf
xffox <dzmitry.ivashniou@disroot.org>
2024-12-04 08:33:33 +0900

    update build scripts

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,5 +1,5 @@
-{ pkgs, xserialization }:
-pkgs.mkShell {
+{ pkgs, stdenv, xserialization }:
+pkgs.mkShell.override {inherit stdenv;} {
   inputsFrom = [ xserialization ];
   packages = with pkgs; [
     pkgs.clang-tools
